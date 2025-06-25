5e390604bceaaff611f92acd31bbff8e39fa2a50
gbrucepayne <gbrucepayne@hotmail.com>
2025-06-25 17:39:39 -0400

    refactor connect method

--- examples/client/basic.py
+++ examples/client/basic.py
@@ -4,7 +4,8 @@ import logging
 import os
 import time
 
-from pyatcommand import AtClient, AtErrorCode
+from pyatcommand import AtClient
+from pyatcommand.common import dprint
 
 LOG_LEVEL = logging.DEBUG
 logfmt = ('%(asctime)s,[%(levelname)s],(%(threadName)s)'
@@ -19,17 +20,19 @@ logging.basicConfig(level=LOG_LEVEL,
                     handlers=[logging.FileHandler('./logs/client.log', mode='w'),
                               console])
 
-logger = logging.getLogger('atclient')
+logger = logging.getLogger()
 
-command_interval = 10   # seconds
+TEST_COMMAND = 'AT'
+TEST_COMMAND_INTERVAL = 10   # seconds
 
 
 def basic_example():
     urc_count = 0
     command_count = 0
     serial_port = os.getenv('SERIAL_PORT', '/dev/ttyUSB0')
+    baudrate = int(os.getenv('SERIAL_BAUDRATE', '9600'))
     modem = AtClient()
-    modem.connect(serial_port)
+    modem.connect(port=serial_port, baudrate=baudrate)
     if modem.is_connected():
         logger.info('Modem connected on %s', serial_port)
         last_cmd = time.time()
@@ -42,18 +45,16 @@ def basic_example():
                     urc_count += 1
                     urc = modem.get_response()
                     logger.info('Got URC: %s', urc)
-                elif time.time() - last_cmd >= command_interval:
+                elif time.time() - last_cmd >= TEST_COMMAND_INTERVAL:
                     command_count += 1
-                    logger.info('Command %d', command_count)
-                    rc = modem.send_at_command('ATI', timeout=-1)
+                    logger.info('Sending command # %d', command_count)
+                    res = modem.send_command(TEST_COMMAND)
                     last_cmd = time.time()
-                    if rc != AtErrorCode.OK:
+                    if not res.ok:
                         logger.warning('Problem sending basic command (%s)',
-                                       modem.last_error_code(True))
-                    if modem.is_response_ready():
-                        response = modem.get_response()
-                        if response:
-                            logger.info('Cleaned response: %s', response)
+                                       dprint(TEST_COMMAND))
+                    elif res.info:
+                        logger.info('Cleaned response: %s', dprint(res.info))
         except KeyboardInterrupt:
             pass
 
